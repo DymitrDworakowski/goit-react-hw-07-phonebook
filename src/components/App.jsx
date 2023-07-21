69b309f18fcaba96ff69ac157da6641915420ee4
DymitrDworakowski <116100679+DymitrDworakowski@users.noreply.github.com>
2023-07-21 14:28:43 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { ContactForm } from './ContactForm /ContactForm';
+import { ContactForm } from './ContactForm/ContactForm';
 import { ContactList } from './ContactList/ContactList';
 import { Filter } from './Filter/Filter';
 import css from './App.module.css';
